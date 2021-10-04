4c9754fe7067b443dc80874dd2b4544509c28ee7
Farhad <afarhad863@gmail.com>
2021-10-04 18:49:59 +0500

    Level 1 encryption

---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+
+const app =express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({
+    extended: true
+}));
+
+//database connection
+const URI = "mongodb://localhost:27017/userDB";
+
+mongoose.connect(URI);
+
+//user schema
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+//user model
+const User = new mongoose.model("User",userSchema);
+
+app.get("/",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    
+    res.render("register");
+});
+
+//register post route
+app.post("/register",(req,res)=>{
+    const newUser = new User({
+        email: req.body.username ,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+
+    
+});
+
+//login post route
+app.post("/login",(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username},(err,foundUser)=>{
+        if(err){
+            console.log("login route: "+error);
+        }
+        else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+});
